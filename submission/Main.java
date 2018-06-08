04f4a7c7f4004607349ddc98fb5f0da4a8204c07
dnacker <davidnackerman@gmail.com>
2018-06-08 09:53:41 -0700

    Fixed Main.java

--- submission/Main.java
+++ submission/Main.java
@@ -5,12 +5,25 @@ public class Main {
         String filename = FileIO.getFileName(args);
         List<City> cities = FileIO.readFile(filename);
         Path tour;
+        StopWatch watch = new StopWatch();
         if (cities.size() < 1000) {
+            watch.start();
             tour = NNAlgorithm.nearestNeighborSolveAllCities(cities);
+            watch.stop();
         } else {
-            tour = NNAlgorithm.nearestNeighborSolveOneCity(cities, cities.get((int)(Math.random()*cities.size())));
+            watch.start();
+            int i = 0;
+            tour = NNAlgorithm.nearestNeighborSolveOneCity(cities, cities.get(i));
+            while (watch.getElapsedTime() < 3 * 60 * 1000 - 1000 && i < cities.size() - 1) {
+                i++;
+                Path currentTour = NNAlgorithm.nearestNeighborSolveOneCity(cities, cities.get(i));
+                if (currentTour.computeLength()< tour.computeLength()) {
+                    tour = currentTour;
+                }
+            }
+            watch.stop();
         }
+        System.out.println("Finished in: " + watch.getElapsedTime() + " ms");
         tour.printPath(System.out);
-        FileIO.writeFile(filename + ".tour", tour);
     }
 }
--- submission/NNAlgorithm.java
+++ submission/NNAlgorithm.java
@@ -2,34 +2,6 @@ import java.util.List;
 import java.util.Scanner;
 
 public class NNAlgorithm {
-    public static void main(String[] args) {
-        String filename = FileIO.getFileName(args);
-        List<City> cities = FileIO.readFile(filename);
-        Path tour;
-        StopWatch watch = new StopWatch();
-        if (cities.size() < 1000) {
-            watch.start();
-            tour = NNAlgorithm.nearestNeighborSolveAllCities(cities);
-            watch.stop();
-        } else {
-            watch.start();
-            int i = 0;
-            tour = NNAlgorithm.nearestNeighborSolveOneCity(cities, cities.get(i));
-            while (watch.getElapsedTime() < 3 * 60 * 1000 - 1000 && i < cities.size() - 1) {
-                i++;
-                Path currentTour = NNAlgorithm.nearestNeighborSolveOneCity(cities, cities.get(i));
-                if (currentTour.computeLength()< tour.computeLength()) {
-                    tour = currentTour;
-                }
-            }
-            watch.stop();
-        }
-        System.out.println("Finished in: " + watch.getElapsedTime() + " ms");
-        tour.printPath(System.out);
-//        FileIO.writeFile(filename + ".tour", tour);
-//        GraphVisualizer graph = new GraphVisualizer(cities, tour);
-    }
-
     public static Path nearestNeighborSolveAllCities(List<City> cities) {
         Path shortestPath = null;
         long shortestPathLength = Long.MAX_VALUE;
